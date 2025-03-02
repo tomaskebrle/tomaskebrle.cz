5a44864bc752db2d9b67368299a8a694f592723c
Tomáš Kebrle <dev@tomaskebrle.cz>
2025-03-02 10:19:58 +0100

    2025 rework

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-![image 5](https://user-images.githubusercontent.com/56836089/219062787-f335efdb-e98a-4ca1-a890-e4437ee100b0.png)
-
 # tomaskebrle.cz
 
 Static blog I built with [Astro](https://astro.build). It uses [TailwindCSS](https://tailwindcss.com) for styling, and markdown files as a simple content management system. On this blog I publish guides to help others, with stuff I already figured out so they don't have to and I document my journey building [school.mesh](https://school.mesh.sk).
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,10 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import remarkToc from "remark-toc";
+import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import remarkCodeTitles from "remark-code-titles";
-import rehypeSlug from "rehype-slug";
-import remarkDirective from "remark-directive";
-import remarkNotes from "./src/utils/notes";
+import compress from "astro-compress";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
-  integrations: [mdx(), sitemap(), tailwind()],
-  markdown: {
-    remarkPlugins: [remarkToc, remarkCodeTitles, remarkNotes, remarkDirective],
-    rehypePlugins: [
-      rehypeSlug,
-      [
-        "rehype-autolink-headings",
-        {
-          behavior: "wrap",
-        },
-      ],
-    ],
-    shikiConfig: {
-      theme: "vitesse-dark",
-      wrap: true,
-    },
-  },
+  integrations: [tailwind(), compress(), robotsTxt()]
 });
